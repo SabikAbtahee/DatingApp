8e8e656f9053be5c548bc713143159e388f28324
SabikAbtahee <sabikchamp@gmail.com>
2022-12-02 01:09:57 +0600

    chore: base api controller made db migrated for new properties

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ builder.Services.AddDbContext<DataContext>(options=>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://datingapp.com:4210"));
 app.MapControllers();
 
 app.Run();
